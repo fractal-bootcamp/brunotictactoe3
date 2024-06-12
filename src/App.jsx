9ccfba8a43d2c24e0ba30b00f27ca2392e921880
brunoLloret <caballoret@gmail.com>
2024-06-12 19:15:21 -0400

    final tictactoe

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,37 @@ export default function Board() {
 
   //Function to handle a click event on the board
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+      // you can do multiple returns on a single function. If this is the case,
+      // the return interrupts whatever is happening down below
+      // before, I created a series of conditions as follows:
+      // if (squares[i] !== (null) && setSquares !== nextSquares)
+    }
+
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    }
+    else {
+      status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+    if (calculateWinner(squares) == true) {
+      console.log(`player ${xIsNext} has won!`)
+    }
+
     //Create a copy of the current squares array
     const nextSquares = squares.slice();
+
+    console.log(nextSquares)
+
+    // another option
+    // if (squares[i] == "X" || squares[i] == "O") {
+
+
+
 
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -43,10 +72,13 @@ export default function Board() {
     //Update the state with the new squares array
     setSquares(nextSquares);
     setXIsNext(!xIsNext)
+
+
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -66,4 +98,23 @@ export default function Board() {
   );
 }
 
-
+// this is imported from the React website, but it was coded in the morning separately
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
